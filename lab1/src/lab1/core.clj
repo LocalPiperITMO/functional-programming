bc7f24a5643b5370180e779739dff4999c4deb69
Local Piper <artsor53@gmail.com>
2024-09-22 10:44:30 +0300

    feat: created src-test template

--- lab1/src/lab1/core.clj
+++ lab1/src/lab1/core.clj
@@ -1,7 +1,47 @@
-(ns lab1.core
-  (:gen-class))
+(ns lab1.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;; Problem 8
+(defn largest-product-in-series-basic-recursion [n]
+  0)
+(defn largest-product-in-series-tail-recursion [n acc]
+  0)
+(defn largest-product-in-series-modular [limit]
+  0)
+(defn largest-product-in-series-map [limit]
+  0)
+(defn largest-product-in-series-loop [limit]
+  0)
+(defn largest-product-in-series-lazy [limit]
+  0)
+
+;; Problem 23
+(defn non-abundant-sums-basic-recursion [n]
+  0)
+(defn non-abundant-sums-tail-recursion [n acc]
+  0)
+(defn non-abundant-sums-modular [limit]
+  0)
+(defn non-abundant-sums-map [limit]
+  0)
+(defn non-abundant-sums-loop [limit]
+  0)
+(defn non-abundant-sums-lazy [limit]
+  0)
+
+(defn -main []
+  ;; Problem 8
+  (println "Problem 8 using Basic Recursion:" (largest-product-in-series-basic-recursion 0))
+  (println "Problem 8 using Tail Recursion:" (largest-product-in-series-tail-recursion 0 0))
+  (println "Problem 8 using Modular Realization:" (largest-product-in-series-modular 0))
+  (println "Problem 8 using Map:" (largest-product-in-series-map 0))
+  (println "Problem 8 using Loop:" (largest-product-in-series-loop 0))
+  (println "Problem 8 using Lazy Collections:" (largest-product-in-series-lazy 0))
+
+  ;; Problem 23
+  (println "Problem 23 using Basic Recursion:" (non-abundant-sums-basic-recursion 0))
+  (println "Problem 23 using Tail Recursion:" (non-abundant-sums-tail-recursion 0 0))
+  (println "Problem 23 using Modular Realization:" (non-abundant-sums-modular 0))
+  (println "Problem 23 using Map:" (non-abundant-sums-map 0))
+  (println "Problem 23 using Loop:" (non-abundant-sums-loop 0))
+  (println "Problem 23 using Lazy Collections:" (non-abundant-sums-lazy 0)))
+
--- lab1/test/lab1/core_test.clj
+++ lab1/test/lab1/core_test.clj
@@ -1,11 +1,26 @@
 (ns lab1.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is testing]]
             [lab1.core :refer :all]))
 
-(deftest a-test
-  (testing "TEST1"
-    (is (= 1 1))
-    (is (= (+ 1 2) 3))
-    (is (= (- 1 1) 0)))
-  (testing "TEST2"
-    (is (= 2 3))))
+(def ans8 23514624000)
+(def ans23 4179871)
+
+(deftest problem8-tests
+  (testing "Project Euler Problem 8: Largest Product in a Series"
+    (is (= ans8 (largest-product-in-series-basic-recursion 0)))
+    (is (= ans8 (largest-product-in-series-tail-recursion 0 0)))
+    (is (= ans8 (largest-product-in-series-modular 0)))
+    (is (= ans8 (largest-product-in-series-map 0)))
+    (is (= ans8 (largest-product-in-series-loop 0)))
+    (is (= ans8 (largest-product-in-series-lazy 0)))))
+
+(deftest problem23-tests
+  (testing "Project Euler Problem 23: Non-Abundant Sums"
+    (is (= ans23 (non-abundant-sums-basic-recursion 0)))
+    (is (= ans23 (non-abundant-sums-tail-recursion 0 0)))
+    (is (= ans23 (non-abundant-sums-modular 0)))
+    (is (= ans23 (non-abundant-sums-map 0)))
+    (is (= ans23 (non-abundant-sums-loop 0)))
+    (is (= ans23 (non-abundant-sums-lazy 0)))))
+
+
