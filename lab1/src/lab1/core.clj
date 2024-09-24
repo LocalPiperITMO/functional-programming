3b6fbcbafb621a4a80cf919fac961361e37a2ec0
Local Piper <artsor53@gmail.com>
2024-09-24 19:25:34 +0300

    feat: added pr23 tail solution

--- lab1/src/lab1/core.clj
+++ lab1/src/lab1/core.clj
@@ -103,7 +103,45 @@
 (def problem-23-input 28123)
 
 (defn non-abundant-sums-basic-recursion [] 0)
-(defn non-abundant-sums-tail-recursion [] 0)
+;; Tail Recursion
+(defn sum-divisors-helper [n i sum]
+  (if (> i (quot n 2))
+    sum
+    (recur n (inc i) (if (zero? (mod n i)) (+ sum i) sum))))
+
+(defn sum-divisors [n]
+  (sum-divisors-helper n 1 0))
+
+(defn abundant-tail-rec? [n]
+  (> (sum-divisors n) n))
+
+(defn find-abundant-numbers-tail-rec [limit i abundants]
+  (if (> i limit)
+    abundants
+    (recur limit (inc i)
+           (if (abundant-tail-rec? i)
+             (conj abundants i)
+             abundants))))
+
+(defn can-be-sum-of-two-abundant-tail-rec? [n abundant-set abundant-nums]
+  (if (empty? abundant-nums)
+    false
+    (let [abundant-num (first abundant-nums)]
+      (if (contains? abundant-set (- n abundant-num))
+        true
+        (recur n abundant-set (rest abundant-nums))))))
+
+(defn non-abundant-sums-tail-recursion []
+  (let [limit 28123
+        abundant-numbers (find-abundant-numbers-tail-rec limit 1 [])
+        abundant-set (set abundant-numbers)]
+    (loop [i 1 total-sum 0]
+      (if (> i limit)
+        total-sum
+        (recur (inc i) (if (can-be-sum-of-two-abundant-tail-rec? i abundant-set (seq abundant-set))
+                         total-sum
+                         (+ total-sum i)))))))
+
 ;; Modular
 (defn generate-sequence [limit]
   (range 1 (inc limit)))
