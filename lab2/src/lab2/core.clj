c8d4dce79a19d3ef12ea12c88587c83e4a076f31
Local Piper <artsor53@gmail.com>
2024-09-29 12:45:34 +0300

    feat: added TDD and templates

--- lab2/src/lab2/core.clj
+++ lab2/src/lab2/core.clj
@@ -1,6 +1,29 @@
 (ns lab2.core
   (:gen-class))
 
+(defn trie-new
+  "Create a new empty Trie."
+  []
+  {})
+
+(defn trie-contains?
+  "Check if a word is present in the Trie."
+  [trie word]
+  (get-in trie (map keyword word) :end))
+
+(defn trie-insert
+  "Add a word to the Trie."
+  [trie word]
+  (assoc-in trie (map keyword word) :end))
+
+(defn trie-remove
+  "Remove a word from the Trie."
+  [trie word]
+  (update-in trie (map keyword word) (constantly nil)))
+
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- lab2/test/lab2/core_test.clj
+++ lab2/test/lab2/core_test.clj
@@ -1,7 +1,36 @@
 (ns lab2.core-test
-  (:require [clojure.test :refer :all]
-            [lab2.core :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [lab2.core :as trie]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; Unit Tests
+(deftest test-add-word
+  (testing "Add word and check if it's present"
+    (is (= true (trie/trie-contains? (trie/trie-insert (trie/trie-new) "hello") "hello")))))
+
+(deftest test-add-multiple-words
+  (testing "Add multiple words and check if they are present"
+    (is (every? #(trie/trie-contains? (trie/trie-insert (trie/trie-new) %) %)
+                ["hello" "world"]))))
+
+(deftest test-remove-word
+  (testing "Add and Remove word"
+    (is (= false (trie/trie-contains? (trie/trie-remove (trie/trie-insert (trie/trie-new) "hello") "hello") "hello")))))
+
+
+;; Property Tests
+(deftest test-associative-property-words
+  (testing "Associative property test (words)"
+    (is (= true false))))
+
+(deftest test-neutral-element-words
+  (testing "Neutral element property test (words)"
+    (is (= (trie/trie-insert (trie/trie-new) "") (trie/trie-new)))))
+
+
+;; Run all tests including unit and property tests
+(deftest all-tests
+  (test-add-word)
+  (test-add-multiple-words)
+  (test-remove-word)
+  (test-associative-property-words)
+  (test-neutral-element-words))
