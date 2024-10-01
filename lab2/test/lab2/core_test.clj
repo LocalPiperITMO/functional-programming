bfad5bbb7eba859adf5f4a0516ee8b09f53dc6e6
Local Piper <artsor53@gmail.com>
2024-10-01 20:30:47 +0300

    feat: finally, number tries

--- lab2/test/lab2/core_test.clj
+++ lab2/test/lab2/core_test.clj
@@ -5,42 +5,62 @@
 ;; Unit Tests
 (deftest test-add-word
   (testing "Add word and check if it's present"
-    (let [test-trie (trie/insert-word (trie/trie-node) "hello")]
-      (is (= true (trie/search-word test-trie "hello"))))
+    (let [test-trie (trie/insert-word (trie/trie-node) "hello")
+          test-number-trie (trie/insert-word (trie/trie-node) 12312)]
+      (is (= true (trie/search-word test-trie "hello")))
+      (is (= true (trie/search-word test-number-trie 12312))))
     ))
 
 (deftest test-add-multiple-words
   (testing "Add multiple words and check if they are present"
-    (let [test-trie (trie/insert-word (trie/insert-word (trie/trie-node) "hello") "world")]
-      (is (every? #(trie/search-word test-trie %) ["hello" "world"]) ))))
+    (let [test-trie (trie/insert-word (trie/insert-word (trie/trie-node) "hello") "world")
+          test-number-trie (trie/insert-word (trie/insert-word (trie/trie-node) 1234) 5678)]
+      (is (every? #(trie/search-word test-trie %) ["hello" "world"]) )
+      (is (every? #(trie/search-word test-number-trie %) [1234 5678])))))
 
 (deftest test-remove-word
   (testing "Add and Remove word"
-    (let [test-trie (trie/remove-word (trie/insert-word (trie/trie-node) "hello") "hello")]
-      (is (= false (trie/search-word test-trie "hello"))))))
+    (let [test-trie (trie/remove-word (trie/insert-word (trie/trie-node) "hello") "hello")
+          test-number-trie (trie/remove-word (trie/insert-word (trie/trie-node) 1234) 1234)]
+      (is (= false (trie/search-word test-trie "hello")))
+      (is (= false (trie/search-word test-number-trie 1234))))))
 
 (deftest test-map-trie
   (testing "Map trie to collection and check if all words are intact"
     (let [test-trie (trie/insert-word (trie/insert-word (trie/trie-node) "hello") "world")
-          trie-map (trie/map-trie test-trie)]
-      (is (= (set ["hello" "world"]) (set trie-map))))))
+          trie-map (trie/map-trie test-trie)
+          test-number-trie (trie/insert-word (trie/insert-word (trie/trie-node) 1234) 5678)
+          number-map (trie/map-trie test-number-trie)]
+      (is (= (set ["hello" "world"]) (set trie-map)))
+      (is (= (set [1234 5678]) (set number-map))))))
 
 (deftest test-map-unmap
   (testing "Map trie to collection, then build a new trie from collection, check 2 tries for equalty"
     (let [trie1 (trie/insert-word (trie/insert-word (trie/trie-node) "hello") "world")
           trie-map (trie/map-trie trie1)
-          trie2 (trie/trie-collection trie-map)]
-      (is (= true (trie/compare-trie trie1 trie2))))))
+          trie2 (trie/trie-collection trie-map)
+          trie3 (trie/insert-word (trie/insert-word (trie/trie-node) 1234) 5678)
+          number-map (trie/map-trie trie3)
+          trie4 (trie/trie-collection number-map)]
+      (is (= true (trie/compare-trie trie1 trie2)))
+      (is (= true (trie/compare-trie trie3 trie4))))))
 
 (deftest test-filter-trie
   (testing "Filter trie, then check if trie is filtered"
     (let [word-list ["hello" "hola" "hi" "aboba" "amogus" "bubus"]
+          number-list [11 123 678 124 16 17 79]
           pr1 #(= \a %)
           pr2 #(pr1 %)
+          pr3 #(even? %)
           trie1 (trie/trie-collection word-list)
           trie2 (trie/filter-trie trie1 pr2)
-          passed-words (filter pr2 word-list)]
-      (is (= (set passed-words) (set (trie/map-trie trie2)))))))
+          trie3 (trie/trie-collection number-list)
+          trie4 (trie/filter-trie trie3 pr3)
+          passed-words (filter pr2 word-list)
+          passed-nums (filter pr3 number-list)
+          ]
+      (is (= (set passed-words) (set (trie/map-trie trie2))))
+      (is (= (set passed-nums) (set (trie/map-trie trie4)))))))
 
 (deftest test-fold-trie
   (testing "Folding tries using different functions"
